1506659d2c31e668c06e4b36802f90386e0a16f5
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-04-01 14:24:11 -0700

    finalized background and layout

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,4 +1,14 @@
-
-$(document).ready(function(){
-  $('nav').localScroll({duration:700});
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
 });
