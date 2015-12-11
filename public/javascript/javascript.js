2f65d26da6cdac50e406b94d9872099cede41b25
Kevin Roger Teng <kevin.roger.teng@gmail.com>
2015-12-11 12:41:58 -0500

    updated js file to redirect to wordpress site

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,14 +1,18 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
-});
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 500);
+//         return false;
+//       }
+//     }
+//   });
+// });
+//redirect for now.
+(function(){
+  window.location.replace('http://kevinrogerteng.com');
+})()
